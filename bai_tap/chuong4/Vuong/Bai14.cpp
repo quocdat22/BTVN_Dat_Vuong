e5a846e18967282acd5897d215f749d904554adf
Vuong <vuongduc914@gmail.com>
2024-08-07 22:15:54 +0700

    chuong4_bai14

---
+++ bai_tap/chuong4/Vuong/Bai14.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+////Bai14
+//using namespace std;
+//int tinhAn(int n) {
+//    if (n == 1) {
+//        return 1;
+//    }
+//    else {
+//        return n * (tinhAn(n - 1) + n - 1);
+//    }
+//}
+//
+//int main() {
+//    int n;
+//    cout << "Nhap n: ";
+//    cin >> n;
+//
+//    cout << "A" << n << " = " << tinhAn(n) << endl;
+//
+//    return 0;
+//}
